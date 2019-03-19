3b98aacf961b9382a8fdc6b6f830ffc87f832a1e
Roger Zhang <roger.zhang@mail.mcgill.ca>
2019-03-19 18:10:45 -0400

    VGA done

--- Lab 4/G28_Lab4/drivers/asm/VGA.s
+++ Lab 4/G28_Lab4/drivers/asm/VGA.s
@@ -7,56 +7,57 @@
 	.global VGA_clear_pixelbuff_ASM
 	.global VGA_clear_charbuff_ASM
 	.global VGA_draw_point_ASM
+	.global VGA_write_byte_ASM
 
 //clear pixel buffer
-
 VGA_clear_pixelbuff_ASM:
-	LDR R1, =CHAR_PIXEL_BASE
-	MOV R2, #0						//Use to clear
-	LDR R3, =320					//x limit 
-	LDR R4, =240					//y	limit
-	MOV R5, #0						//x counter
-	MOV R6, #0						//y counter
+	LDR R3, =CHAR_PIXEL_BASE
+	MOV R4, #0		//Move 0 into register to clear
+	MOV R5, #1 		//x counter
+	MOV R6, #1		//y counter
 	
-X_LOOP:
-	CMP R5, R3
-	BEQ Y_LOOP
-	ADD R7, R5, R6, LSL #9			//R7 contains the offset
-	LSL R7, #1						
-	ADD R7, R7, R1					//Add offset to base adress
-	STRH R5, [R1]					//Clear adress
-	ADD R5, R5, #1
-	B X_LOOP
-	
-Y_LOOP:
-	CMP R6, R4
-	BEQ END
-	MOV R5, #0
+ClearPixelBuffLoopX:
+	CMP R5, #320			
+	BGT ClearPixelBuffLoopY		//if 320 iterations passed, loop to next y
+	STRH R4, [R3]			//store black colour at proper pixel
+	ADD R3, R3, #2			//increment by 2 to go to next x address
+	ADD R5, R5, #1			//add 1 to x counter
+	B ClearPixelBuffLoopX
+		
+ClearPixelBuffLoopY:
+	CMP R6, #240			//counter for y-axis
+	BGT END	
+	MOV R5, #1			//reset x counter
+	LDR R3, =CHAR_PIXEL_BASE 	//reset r3 to base address
+	MOV R7, R6				//load counter of y inside r7
 	ADD R6, R6, #1
-	B X_LOOP
+	ADD R3, R3, R7, LSL #10		//add value of y to base address	
+	B ClearPixelBuffLoopX
 
 
 //clear character buffer
 VGA_clear_charbuff_ASM:
 	LDR R3, =CHAR_CHAR_BASE
-	LDR R5, =CHAR_END
-	MOV R4, #0		//Move 0 into adress to clear
-	MOV R6, #79		//Counter for x
+	MOV R4, #0				//Move 0 into adress to clear
+	MOV R5, #1
+	MOV R6, #1		
 
 ClearCharBuffLoopX:
-	STRB R4, [R3]
-	ADD R3, R3, #1			
-	SUBS R6, R6, #1
-	BGE ClearCharBuffLoopX
-	B ClearCharBuffLoopY
+	CMP R5, #80			
+	BGT ClearCharBuffLoopY		//if 320 iterations passed, loop to next y
+	STRB R4, [R3]			//store black colour at proper pixel
+	ADD R3, R3, #1			//increment by 2 to go to next x address
+	ADD R5, R5, #1			//add 1 to x counter
+	B ClearCharBuffLoopX
 
 ClearCharBuffLoopY:
-	CMP R3, R5  //compare if we reach end point
-	BEQ END     //if we are at endpoint, quit 
-	MOV R8, #1
-	SUB R3, R3, #80
-	ADD R3, R3, R8, LSL #7
-	MOV R6, #79		//Counter for x
+	CMP R6, #60					//counter for y-axis
+	BGT END	
+	MOV R5, #1					//reset x counter
+	LDR R3, =CHAR_CHAR_BASE 	//reset r3 to base address
+	MOV R7, R6					//load counter of y inside r7
+	ADD R6, R6, #1
+	ADD R3, R3, R7, LSL #7		//add value of y to base address	
 	B ClearCharBuffLoopX
 
 VGA_write_char_ASM:
@@ -78,7 +79,51 @@ STORE_CHAR:
 	STRB R2, [R5]				//Store value in adress
 	B END
 
+VGA_write_byte_ASM:
+	LDR R3, =CHAR_CHAR_BASE		//R3 contains char buff base address
+	CMP R0, #79					//Check if x value if ok
+	BGT END
+	CMP R0, #0
+	BMI END
+	CMP R1, #59					//Check if y value is ok
+	BGT END
+	CMP R1, #0
+	BMI END
+	MOV R6, R2					//Load value to display in R6
+	
+FIND_BYTE_ADRESS:
+	ADD R4, R0, R1, LSL #7		//Shift y and add to x
+	ADD R5, R3, R4				//Add offset to base address
 
+FIND_FIRST_HEX:
+	LSR R6, #4					//Get the first 4 bit 
+	AND R6, R6, #0xF
+	CMP R6, #10					//Check if value is a letter
+	BGE FIRST_LETTER
+	ADD R7, R6, #0x30			//R7 Store ascii value of the number
+	B STORE_FIRST_HEX
+	
+FIRST_LETTER:
+	ADD R7, R6, #0x37			//0X37 + offset of 10 gives starting ascii of 0x041 (letter A) 
+
+STORE_FIRST_HEX:
+	STRB R7, [R5]					//Store first hex
+	ADD R5, R5, #1				//Go to address of second hex
+	MOV R6, R2
+
+FIND_SECOND_HEX:
+	AND R6, R6, #0xF
+	CMP R6, #10					//Check if value is a letter
+	BGE SECOND_LETTER
+	ADD R7, R6, #0x30			//R7 Store ascii value of the number
+	B STORE_SECOND_HEX
+
+SECOND_LETTER: 
+	ADD R7, R6, #0x37
+	
+STORE_SECOND_HEX:
+	STRB R7, [R5]					//Store first hex
+	B END
 
 VGA_draw_point_ASM:
 	LDR R3, =CHAR_PIXEL_BASE		//R3 contains pixel buff base adress
--- Lab 4/G28_Lab4/vga_test.c
+++ Lab 4/G28_Lab4/vga_test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-#include "./drivers/inc/VGA.h"			
+#include "./drivers/inc/VGA.h"	
+#include "./drivers/inc/pushbuttons.h"
+#include "./drivers/inc/slider_switches.h"		
 
 void test_char(){
 	int x, y;
@@ -9,6 +11,17 @@ void test_char(){
 	for(y = 0; y <= 59; y++){
 		for(x = 0; x <= 79; x++){
 			VGA_write_char_ASM(x, y, c++);		
+		}
+	}
+}
+
+void test_byte(){
+	int x, y;
+	char c = 0;
+	
+	for(y = 0; y <= 59; y++){
+		for(x = 0; x <= 79; x+=3){
+			VGA_write_byte_ASM(x, y, c++);
 		}
 	}
 }
@@ -27,12 +40,37 @@ void test_pixel(){
 }
 
 int main () {
-	
-	//test_pixel();
-	//test_char();
-	VGA_clear_pixelbuff_ASM();
 
-	//VGA_clear_charbuff_ASM();
+while(1){
+	//If any switch is on, use test byte
+	if(read_slider_switches_ASM()){
+		if(PB_edgecap_is_pressed_ASM(PB0)){
+			test_byte();
+			PB_clear_edgecap_ASM(PB0);
+		} 
+	} else {									//Otherwise, use test char
+		if(PB_edgecap_is_pressed_ASM(PB0)){
+			test_char();
+			PB_clear_edgecap_ASM(PB0);
+		} 
+	}
+	
+	if(PB_edgecap_is_pressed_ASM(PB1)){			//If button 1 is pressed, test pixel
+			test_pixel();
+			PB_clear_edgecap_ASM(PB1);
+	}
+	if(PB_edgecap_is_pressed_ASM(PB2)){			//If button 2 is pressed, test pixel
+			VGA_clear_pixelbuff_ASM();
+			PB_clear_edgecap_ASM(PB2);
+		} 
+	if(PB_edgecap_is_pressed_ASM(PB3)){			//If button 3 is pressed, test pixel
+			VGA_clear_charbuff_ASM();
+			PB_clear_edgecap_ASM(PB3);
+		}
+
+	//PB_clear_edgecap_ASM(PB0 | PB1 | PB2 | PB3); 
+	
+}
 
 	return 0;
 
